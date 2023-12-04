ffda9d5846f7d72d86af3e9688ea1a0422275973
Nguyễn Chí Thuận <83408110+nguyenchithuan2311@users.noreply.github.com>
2023-12-04 14:59:25 +0700

    Add files via upload

---
+++ Ascenda_Exercise/Ascenda_Exercise/Filter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Filter
+    {
+        public List<Offer> dateFilter(OfferData offerDatas, DateOnly date)
+        {
+            
+            List<Offer> offerDatasNew=new List<Offer>();
+            foreach (Offer offer in offerDatas.Offers)
+            {
+                DateOnly dt = DateOnly.Parse(offer.valid_to);
+                DateOnly valid_date = date.AddDays(5);
+                if (valid_date <= dt)
+                {
+                    offerDatasNew.Add(offer);
+                }
+            }
+            return offerDatasNew;
+        }
+        public void categoryFilter(List<Offer> offerDatas, List<Restaurant> restaurant, List<Retail> retail, List<Activity> activity)
+        {
+            foreach (Offer offer in offerDatas)
+            {
+                foreach (merchants merchant in offer.merchants)
+                {
+                    if (offer.category == 1)
+                    {
+                        Restaurant restaurantTemp = new Restaurant(merchant);
+                        restaurantTemp.id = offer.id;
+                        restaurant.Add(restaurantTemp);
+                    }
+                    else if (offer.category == 2)
+                    {
+                        Retail retailTemp = new Retail(merchant);
+                        retailTemp.id = offer.id;
+                        retail.Add(retailTemp);
+                    }
+                    else if (offer.category == 4)
+                    {
+                        Activity activityTemp = new Activity(merchant);
+                        activityTemp.id = offer.id;
+                        activity.Add(activityTemp);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Ascenda_Exercise/Ascenda_Exercise/Model.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+public class OfferData
+{
+    public List<Offer> Offers { get; set; }
+}
+public class Offer
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public int category { get; set; }
+    public List<merchants> merchants { get; set; }
+    public string valid_to { get; set; }
+        /*": "2020-02-01"*/
+    }
+public class merchants
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public float distance { get; set; }
+}
+
+public class Restaurant: merchants
+{
+    public Restaurant(merchants merchant) {
+        this.id = merchant.id;
+        this.name = merchant.name;
+        this.distance = merchant.distance;
+    }
+    private int idOffer {  get; set; }
+}
+public class Retail : merchants
+{
+    public Retail(merchants merchants)
+    {
+        this.id = merchants.id;
+        this.name = merchants.name;
+        this.distance = merchants.distance;
+    }
+    private int idOffer { get; set; }
+}
+public class Activity : merchants
+{
+    public Activity(merchants merchants)
+    {
+        this.id = merchants.id;
+        this.name = merchants.name;
+        this.distance = merchants.distance;
+    }
+    private int idOffer { get; set; }
+}
